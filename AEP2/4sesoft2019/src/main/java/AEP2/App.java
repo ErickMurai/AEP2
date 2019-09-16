3aad16681212ff83fc6c53e7cc479845871915de
ErickMurai <51099566+ErickMurai@users.noreply.github.com>
2019-09-15 22:43:53 -0300

    Update App.java

--- AEP2/4sesoft2019/src/main/java/AEP2/App.java
+++ AEP2/4sesoft2019/src/main/java/AEP2/App.java
@@ -6,14 +6,12 @@ public class App {
 	public static void main(String[] args) {
 		Aluno a1 = new Aluno("A1", "Paulo", 5.00d);
 		Aluno a2 = new Aluno("A2", "Verena", 9.00d);
-		Aluno a3 = new Aluno("A3", "Gabriel", 4.00d);
-		Aluno a4 = new Aluno("A4", "Samy", 5.00d);
+		
 		
 		Turma t1 = new Turma("esoft1");
 		t1.adicionarAluno(a1);
 		t1.adicionarAluno(a2);
-		t1.adicionarAluno(a3);
-		t1.adicionarAluno(a4);
+		
 		
 		imprimirAlunoDaTurmaNoConsole(t1);
 	}
